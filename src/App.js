1dcc17f9ad97fa4ea89f79b36f220fab82e1e116
jwblau2 <jeremy@abundantly.com>
2023-11-09 21:31:24 -0700

    added formatting to the Nav Menu

--- src/App.js
+++ src/App.js
@@ -1,20 +1,30 @@
 import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import NavMenu from './components/NavMenu';
 import Home from './pages/home';
 import About from './pages/about';
 import Projects from './pages/projects';
-import Contacts from './pages/contact';
+import Contact from './pages/contact';
 
 export default function App() {
   return (
     <>
       <Router>
         <NavMenu />
-        <Home />
-        <About />
-        <Projects />
-        <Contacts />
+        <Switch>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/projects">
+            <Projects />
+          </Route>
+          <Route path="/contact">
+            <Contact />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
       </Router>
     </>
   );
