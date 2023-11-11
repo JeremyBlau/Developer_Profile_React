d34ee0651b371afdd81bf8b738bbe0f16b97bd65
jwblau2 <jeremy@abundantly.com>
2023-11-11 14:18:20 -0700

    added profile pic

---
+++ src/components/HeroSection.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import HeroImg from '../assets/images/hero.png';
+
+export default function HeroSection() {
+  return (
+    <div>
+      <div className="container">
+        <h1 className="hero__heading">
+          <span>Hello, This is</span>
+          <span>Jeremy Blau</span>
+        </h1>
+        <div className="hero__img">
+          <img src={HeroImg} alt="" />
+        </div>
+      </div>
+    </div>
+  );
+}
