b22e551a6eb2d9e6a48ebeb9854f83e0aa5bd47d
jwblau2 <jeremy@abundantly.com>
2023-11-11 20:52:14 -0700

    added to the resume page

--- src/pages/resume.js
+++ src/pages/resume.js
@@ -1,15 +1,17 @@
 import React from 'react';
+import myResume from '../assets/resume/Resume_Jeremy Wallace Blau.pdf';
+import '../styles/resume.css';
 
 export default function Resume() {
   // Function to handle the download action
   const handleDownload = () => {
-    // Replace 'your_resume.pdf' with the actual filename of your resume
-    const resumeUrl = '/path/to/your_resume.pdf';
+    // Use the imported resume file
+    const resumeUrl = myResume;
 
     // Create an anchor element with the download attribute
     const link = document.createElement('a');
     link.href = resumeUrl;
-    link.download = 'your_resume.pdf';
+    link.download = 'Resume_Jeremy Wallace Blau.pdf';
 
     // Append the anchor element to the body
     document.body.appendChild(link);
@@ -22,18 +24,39 @@ export default function Resume() {
   };
 
   return (
-    <div>
-      <header>
-        <h1>Your Name</h1>
-        <p>Web Developer</p>
-      </header>
+    <div className="resume-container">
+      <section>
+        <h2>Experience</h2>
+        <div className="experience-container">
+          <div className="experience-column">
+            <h3>Front-end Experience</h3>
+            <ul>
+              <li>HTML</li>
+              <li>CSS</li>
+              <li>JavaScript</li>
+              <li>React</li>
+              <li>PWA</li>
+              <li>Webpack</li>
+            </ul>
+          </div>
 
-      {/* ... (previous sections) */}
+          <div className="experience-column">
+            <h3>Back-end Experience</h3>
+            <ul>
+              <li>Node.js</li>
+              <li>Express.js</li>
+              <li>Database (e.g., MongoDB)</li>
+              <li>MySQL</li>
+              <li>RESTful API Design</li>
+            </ul>
+          </div>
+        </div>
+      </section>
 
       <section>
-        <h2>Download Resume</h2>
+        <h2>Click to Download My Resume</h2>
         <button type="button" onClick={handleDownload}>
-          Download
+          Resume
         </button>
       </section>
     </div>
